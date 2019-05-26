111449973ba4483d1581606ef7f0fcc13176ced1
Sifan Jiang <jasper.sifan@gmail.com>
2019-05-26 20:30:11 +0800

    a1 accomplished

--- Assignment/assignment_1_q_2.R
+++ Assignment/assignment_1_q_2.R
@@ -1,5 +1,18 @@
-p = seq(0, 1, length = 10000);
-likelihood = pbinom(2, size = 10, prob = p); prior = qbeta(p, 4, 4);
-posterior = likelihood * prior;
-plot(p, posterior, xlim = c(0, 1), type = "l", col = "red", xlab = "p", ylab = "Posterior Probability");
-grid();
+p = seq(0, 1, length = 10000)
+
+likelihood = pbinom(2, size = 10, prob = p) / 3
+prior = qbeta(p, 4, 4)
+
+posterior = likelihood * prior
+
+plot(
+  p,
+  posterior,
+  xlim = c(0, 1),
+  type = "l",
+  col = "red",
+  xlab = "p",
+  ylab = "Posterior Probability"
+)
+
+grid()
