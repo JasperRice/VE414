2790699f1b62b17e70c994b6d5900ec5fb2bc6ed
gengzichen <2020905037@qq.com>
2019-05-28 16:03:48 +0800

    A1 finished.

--- Assignment/assignment_1_q_2.R
+++ Assignment/assignment_1_q_2.R
@@ -1,13 +1,15 @@
 p = seq(0, 1, length = 10000)
 
-likelihood = pbinom(2, size = 10, prob = p) / 3
-prior = qbeta(p, 4, 4)
+# likelihood = pbinom(2, size = 10, prob = p) / 3
+# prior = qbeta(p, 4, 4)
+# posterior = likelihood * prior
 
-posterior = likelihood * prior
+posterior = p^(3) * (1 - p)^(13) + 10 * p^(4) * (1 - p)^(12) + 45 * p^(5) * (1 - p)^(11)
+c = 7735 / 8
 
 plot(
   p,
-  posterior,
+  c * posterior,
   xlim = c(0, 1),
   type = "l",
   col = "red",
@@ -16,3 +18,4 @@ plot(
 )
 
 grid()
+
