6847a160d8caf122ee0e7bac80a7b12bcfb082e2
Sifan Jiang <jasper.sifan@gmail.com>
2019-07-22 17:23:10 +0800

    Working on Project

--- Project/test.jl
+++ Project/test.jl
@@ -1,14 +1,11 @@
-using Pkg
-Pkg.add("PyPlot")
+# using Pkg
+# Pkg.add("PyPlot")
 
-using CSV
-using DataFrames
-using DataFramesMeta
-using Query
-
+using CSV, DataFrames, DataFramesMeta, Query
+using Distances
 using Distributions
 using Plots
-using PyPlot
+# using PyPlot
 
 function UniformCircle(X, Y, R = 3)
     θ = rand(Uniform(0, 2*pi))
@@ -18,32 +15,95 @@ function UniformCircle(X, Y, R = 3)
     return [x, y]
 end
 
-FilePath = "C:\\Users\\Jasper Rice\\Desktop\\VE414\\Project\\data_proj_414.csv"
-DF = CSV.File(FilePath) |> DataFrame
+function EuclideanDistance(X, Y)
+    if size(X) == size(Y)
+        Dist = 0
+        if size(X,1) == 1
+            for i = 1 : size(X, 2)
+                Dist += (X[i] -Y[i])^2
+            end
+            return sqrt(Dist)
+        else
+            for i = 1 : size(X, 1)
+                Dist += (X[i] -Y[i])^2
+            end
+            return sqrt(Dist)
+        end
+    else
+        print("Wrong size of array when calculating Euclidean distance.")
+        exit()
+    end
+end
 
-POTTER = @from i in DF begin
+function IsDifferentTree(X, Y, Threshold = 1)
+    if EuclideanDistance(X, Y) > Threshold
+        return true
+    else
+        return false
+    end
+end
+
+############### <BEGIN> Data collection ###############
+FILEPATH = "C:\\Users\\Jasper Rice\\Desktop\\VE414\\Project\\Data.csv"
+DF = CSV.File(FILEPATH) |> DataFrame
+
+POTTER_ALL = @from i in DF begin
     @where i.Potter == 1
     @select {i.X, i.Y, i.Trip, i.Close, i.Far}
     @collect DataFrame
 end
+POTTER = Array{DataFrame, 1}(undef, 45)
+for n = 1 : size(POTTER, 1)
+    POTTER[n] = @from i in POTTER_ALL begin
+        @where i.Trip == n
+        @select {i.X, i.Y, i.Close, i.Far}
+        @collect DataFrame
+    end
+end
 
-WEASLEY = @from i in DF begin
+WEASLEY_ALL = @from i in DF begin
     @where i.Weasley == 1
     @select {i.X, i.Y, i.Trip, i.Close, i.Far}
     @collect DataFrame
 end
+WEASLEY = Array{DataFrame, 1}(undef, 47)
+for n = 1 : size(WEASLEY, 1)
+    WEASLEY[n] = @from i in WEASLEY_ALL begin
+        @where i.Trip == n
+        @select {i.X, i.Y, i.Close, i.Far}
+        @collect DataFrame
+    end
+end
 
-GRANGER = @from i in DF begin
+GRANGER_ALL = @from i in DF begin
     @where i.Granger == 1
     @select {i.X, i.Y, i.Trip, i.Close, i.Far}
     @collect DataFrame
 end
+GRANGER = Array{DataFrame, 1}(undef, 43)
+for n = 1 : size(GRANGER, 1)
+    GRANGER[n] = @from i in GRANGER_ALL begin
+        @where i.Trip == n
+        @select {i.X, i.Y, i.Close, i.Far}
+        @collect DataFrame
+    end
+end
+############### <END> Data collection ###############
 
-Plots.plot(DF.X, DF.Y, DF.Close, xlim=(0,100), ylim=(0,100), zlim=(0,20), seriestype=:scatter, marker=1)
-PlotPath = "C:\\Users\\Jasper Rice\\Desktop\\VE414\\Project\\Plot"
-savefig(PlotPath)
+
+
+Plots.plot(DF.X, DF.Y, DF.Close,
+    xlim=(0,100), ylim=(0,100), zlim=(0,20),
+    title="3D Plot", seriestype=:scatter, marker=1)
+PLOTPATH = "C:\\Users\\Jasper Rice\\Desktop\\VE414\\Project\\Plot"
+Plots.savefig(PLOTPATH)
+
+# GIF = @animate for i=1:120
+#     Plots.plot(DF.X[i], DF.Y[i], DF.Close[i])
+# end
+# gif(GIF, "C:\\Users\\Jasper Rice\\Desktop\\VE414\\Project\\GIF.gif", fps=60)
 
 # Gif = plot3d(1, xlim=(0, 100), ylim=(0,100), zlim=(0,20), title="Close Tayes Number", marker=2)
-# @gif for i=1:size(DF,1)
+# @gif for i=1:100
 #     push!(plt, DF.X, DF.Y, DF.Close)
 # end every 1
